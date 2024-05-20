046913790293bfe5877838666cb80d1a2321593e
ovidiu-vasilescu <ovidiu.vasilescu94@gmail.com>
2024-05-20 17:12:24 +0300

    test trivy

---
+++ app.js
@@ -0,0 +1,4 @@
+// app.js
+
+const dbPassword = 'SuperSecretPassword';
+const apiKey = 'abcdef1234567890';
---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+
+DATABASE_URL = "postgres://ovidiu:testpassword@localhost:5432/mydatabase"
+AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
+AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
